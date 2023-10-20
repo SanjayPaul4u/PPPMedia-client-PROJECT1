b8feef87389fa2b282207960080c3bda949c7475
SanjayPaul4u <sanjoypaul655@gmail.com>
2023-10-20 19:45:30 +0530

    10: Work on Login Component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import "../styleFolder/Login.css"
+import LoginImg from './images/log-in.png'
 
 function Login() {
   return (
-    <div className='container'>Login
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus aliquam rerum dicta atque at incidunt non, velit modi recusandae debitis assumenda molestiae perferendis dolorem provident nemo repudiandae, cupiditate placeat architecto praesentium illo tempore. Saepe molestiae laudantium natus eum iste soluta assumenda temporibus? Totam et ullam maxime ab? Provident, unde expedita?</p></div>
+    <div id='main-login-component' className='container'>
+        <div id='login-component'>
+            <h1 id='login-header'>Login To Use PPP Media</h1>
+            <hr />
+
+            <div className="row">
+                <div id="login-img-div" className="col-6 col-md-6 col-xl-6 text-center">
+                  <img src={LoginImg} alt="imgError" />
+                </div>
+                <div id="login-form-div" className="col-6 col-md-6 col-xl-6">
+                  {/* login form start*/}
+
+                    <form>
+                      <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email'/>
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" name='password'/>
+                      </div>
+                      <button type="submit" className="btn btn-primary">Login</button>
+
+                        <div>
+                          <h6 className="my-2">If You Don't Have Account Then <a href="/signup" role="button">SignUp</a></h6>
+                        </div>
+                    </form>
+
+                  {/* login form end*/}
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
