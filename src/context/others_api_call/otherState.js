6d1282ae1a3c9edaaed8e592b7bddc2c3d9ec790
root <root@ubuntu-cloudpanel.localhost>
2023-11-09 16:22:09 +0000

    client updated

--- src/context/others_api_call/otherState.js
+++ src/context/others_api_call/otherState.js
@@ -103,3 +103,4 @@ const OtherState = (props) =>{
 }
 
 export default OtherState;
+
--- src/hooks/setCookie.js
+++ src/hooks/setCookie.js
@@ -3,9 +3,10 @@ import Cookie from 'js-cookie';
 const SetCookie = (cookieName, value)=>{
     Cookie.set(cookieName, value, {
         // expires: 1/48,// 30 minute // 10 minute(144) - "here 1 is day(1440minute)/ minute(144)"
+        //domain: 'www.pppmedia.com',
         expires: 1,// 1 day
         secure: true,
-        sameSite : "strict",
+        sameSite : "none",
         path: "/"
     })
 }
